7c0d244175c5ce622cbe540d95d42589353db76c
caglaralkiss <caglaralkiss@gmail.com>
2022-03-03 06:39:17 +0300

    implements inject and extract functions

--- src/__tests__/index.test.ts
+++ src/__tests__/index.test.ts
@@ -1,5 +1,86 @@
-describe('Index', () => {
-	test('should run the dummy case', () => {
-		expect(1).toBe(1)
+import { extract, inject } from '../index'
+
+describe('Stega', () => {
+	test('should inject source content to target by replacing LSB of each target byte', () => {
+		const sourceBuff = Buffer.from([0b01100101, 0b00000101])
+		const targetBuff = Buffer.from([
+			0b00000000,
+			0b00000000,
+			0b00000000,
+			0b00000000,
+			0b00000000,
+			0b00000000,
+			0b00000000,
+			0b00000000,
+			0b00000000,
+			0b00000000,
+			0b00000000,
+			0b00000000,
+			0b00000000,
+			0b00000000,
+			0b00000000,
+			0b00000000,
+			0b00000000,
+			0b00000000
+		])
+
+		const resultBuff = Buffer.from([
+			0b00000000,
+			0b00000001,
+			0b00000001,
+			0b00000000,
+			0b00000000,
+			0b00000001,
+			0b00000000,
+			0b00000001,
+			0b00000000,
+			0b00000000,
+			0b00000000,
+			0b00000000,
+			0b00000000,
+			0b00000000,
+			0b00000001,
+			0b00000000,
+			0b00000001,
+			0b00000001
+		])
+
+		expect(inject(sourceBuff, targetBuff)).toEqual(resultBuff)
+	})
+
+	test('should throw error when target buffer length is not enough for source steganography', () => {
+		const sourceBuff = Buffer.from([0xFE])
+		const targetBuff = Buffer.from([0xFF, 0xF1, 0xA9, 0xFF, 0xF1, 0xA9, 0xF3, 0x21])
+
+		expect(() => inject(sourceBuff, targetBuff)).toThrowError()
+	})
+
+	test('should extract original buffer from stego buffer', () => {
+		const stegoBuff = Buffer.from([
+			0b00000000,
+			0b00000001,
+			0b00000001,
+			0b00000000,
+			0b00000000,
+			0b00000001,
+			0b00000000,
+			0b00000001,
+			0b00000000,
+			0b00000000,
+			0b00000000,
+			0b00000000,
+			0b00000000,
+			0b00000000,
+			0b00000001,
+			0b00000000,
+			0b00000001,
+			0b00000001
+		])
+
+		console.log('stegoBuff', extract(stegoBuff))
+
+		const originalBuff = Buffer.from([0b01100101, 0b00000101])
+		console.log('origiBuff', originalBuff)
+		expect(extract(stegoBuff)).toEqual(originalBuff)
 	})
 })
---
+++ src/index.ts
@@ -0,0 +1,78 @@
+/**
+ *
+ * Inject source content to given target buffer by replacing each LSB of target byte.
+ *
+ * @param source Injection buffer
+ * @param target LSB byte replacing buffer
+ * @returns stegoBuffer Target buffer that is replaced each bytes LSB
+ */
+export function inject(source: Buffer, target: Buffer): Buffer {
+	const srcByteLength = Buffer.byteLength(source)
+	const targetByteLength = Buffer.byteLength(target)
+
+	if (srcByteLength * 9 > targetByteLength) {
+		throw new Error(`Target size length must be greater or equal then ${srcByteLength * 9}`)
+	}
+
+	let index = 0
+
+	for (let i = 0; i < source.length; i++) {
+		for (let j = 7; j >= 0; j--) {
+			const lsb = (source[i] >> j) & 0x01
+
+			target[index] = target[index] & 0xFE | lsb
+			++index
+		}
+
+		if (i < source.length - 1) {
+			target[index] = target[index] & 0xFE | 0
+			++index
+		} else {
+			target[index] = target[index] & 0xFE | 1
+		}
+	}
+
+	return target
+}
+
+/**
+ *
+ * Extract original buffer from given stego buffer
+ *
+ * @param stego Stego buffer that includes a hidden content in LSB of some byte
+ * @returns originalBuffer Buffer that reveals hidden content
+ */
+export function extract(stego: Buffer): Buffer {
+	const lsbArr: Array<Array<number>> = []
+
+	let indexCounter = 0
+	let shiftSize = 7
+	let byteIndex = 0
+
+	for (const b of stego) {
+		if ((indexCounter + 1) % 9 !== 0) {
+			if (!lsbArr[byteIndex]) {
+				lsbArr.push([])
+			}
+
+			lsbArr[byteIndex].push((b & 0x01) << shiftSize)
+			--shiftSize
+		} else {
+			if (b & 0x01) {
+				break
+			} else {
+				shiftSize = 7
+				++byteIndex
+			}
+		}
+
+		++indexCounter
+	}
+
+	const bytes = lsbArr
+					.map(lsb => lsb.reduce((acc, curr) => acc + curr, 0))
+					.flat()
+
+	return Buffer.from(bytes)
+}
+
